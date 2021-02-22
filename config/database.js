df0630997a34347828c11453d50c1183a1f37511
Paul <53581849+PICXMANIA@users.noreply.github.com>
2021-02-22 17:46:57 +0300

    Db file added

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize('rejic','root','',{
+    host:'localhost',
+    dialect:'mysql',
+    operatorAliases:0,
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    },
+});
